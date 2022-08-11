f9b413ac8fb045d15044d3af8dc9ebfb39ccea2e
coole <83263997+dcee-py@users.noreply.github.com>
2022-08-11 14:40:00 -0400

    Add search feature

--- src/main/java/com/revature/models/Post.java
+++ src/main/java/com/revature/models/Post.java
@@ -11,9 +11,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Entity
--- src/main/java/com/revature/repositories/UserRepository.java
+++ src/main/java/com/revature/repositories/UserRepository.java
@@ -2,10 +2,16 @@ package com.revature.repositories;
 
 import com.revature.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+import java.util.ArrayList;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmailAndPassword(String email, String password);
+
+    Optional<ArrayList<User>> findAllByFirstNameContaining(String name);
 }
