5736eda24bd22490f2c96543c789ab3ccf641fd8
BarritoN78 <95886922+BarritoN78@users.noreply.github.com>
2022-08-11 12:47:00 -0400

    Added custom responses to AuthController with exceptions

--- src/main/java/com/revature/services/AuthService.java
+++ src/main/java/com/revature/services/AuthService.java
@@ -1,5 +1,7 @@
 package com.revature.services;
 
+import com.revature.exceptions.EmailAlreadyExistsException;
+import com.revature.exceptions.UsernameAlreadyExistsException;
 import com.revature.models.User;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +21,7 @@ public class AuthService {
         return userService.findByCredentials(email, password);
     }
 
-    public User register(User user) {
+    public User register(User user) throws EmailAlreadyExistsException, UsernameAlreadyExistsException {
         return userService.save(user);
     }
 }
