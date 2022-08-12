81f06c30ac59239a0bb93adb8086936ca8711fac
coole <83263997+dcee-py@users.noreply.github.com>
2022-08-12 15:17:59 -0400

    changes

--- src/main/java/com/revature/dtos/LoginRequest.java
+++ src/main/java/com/revature/dtos/LoginRequest.java
@@ -5,13 +5,10 @@ import lombok.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
+
 public class LoginRequest {
-    @Getter
-    @Setter
+
     private String email;
-    @Getter
-    @Setter
+
     private String password;
 }
