8ef4f3f2ed621ec15c2aea54c7b62f0997723b28
BarritoN78 <95886922+BarritoN78@users.noreply.github.com>
2022-08-15 16:50:25 -0400

    Connected front-end to database

--- src/main/java/com/revature/repositories/UserRepository.java
+++ src/main/java/com/revature/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.revature.dtos.UserDTO;
 import com.revature.models.User;
 
 @Repository
