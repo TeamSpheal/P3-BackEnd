baff6c66daf51f840280a62cde4eae7437c9f916
Colby Tang <cobtang@gmail.com>
2022-08-17 10:06:40 -0500

    Added Michael's CORS config, allowCredentials=true

--- src/main/java/com/revature/SocialMediaApplication.java
+++ src/main/java/com/revature/SocialMediaApplication.java
@@ -3,6 +3,9 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan("com.revature.models")
@@ -22,5 +25,19 @@ public class SocialMediaApplication {
 "-------------------------------------------------------------------------------------------------\n"
 		);
 		System.out.println("Social Media Application is now running! Press the Red Stop Button in Eclipse to shutdown or CTRL+C in other IDEs.");
-	}	
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+					.allowedOrigins()
+					.allowedHeaders("*")
+					.exposedHeaders("Auth")
+					.allowCredentials(true);
+			}
+		};
+	}
 }
