3259b0af84636dcb905d14491f68c6aeb2e47c6d
Colby Tang <cobtang@gmail.com>
2022-08-19 18:05:50 -0500

    Fixed SearchController

--- src/main/java/com/revature/controllers/SearchController.java
+++ src/main/java/com/revature/controllers/SearchController.java
@@ -1,11 +1,14 @@
 package com.revature.controllers;
 
 import com.revature.dtos.UserDTO;
+import com.revature.models.User;
 import com.revature.services.SearchServiceImpl;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,8 +27,8 @@ public class SearchController {
      * @return ResponseEntity<ArrayList<User>>
      */
     @GetMapping("/{name}")
-    protected ResponseEntity<ArrayList<User>> getByName(@PathVariable String name) {
-        Optional<ArrayList<User>> oUserList = searchServ.queryUserTable(name);
+    protected ResponseEntity<List<UserDTO>> getByName(@PathVariable String name) {
+        Optional<List<UserDTO>> oUserList = searchServ.queryUserTable(name);
         if (oUserList.isPresent()) {
             return ResponseEntity.ok(oUserList.get());
         }
