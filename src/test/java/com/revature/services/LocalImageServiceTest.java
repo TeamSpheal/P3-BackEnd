aca3450bc00f25585f5cf155f7efcb547c098cab
Colby Tang <cobtang@gmail.com>
2022-08-23 18:08:48 -0500

    Fixing public modifier code smell

--- src/test/java/com/revature/services/LocalImageServiceTest.java
+++ src/test/java/com/revature/services/LocalImageServiceTest.java
@@ -1,10 +1,6 @@
 package com.revature.services;
 
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,23 +9,17 @@ import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.revature.exceptions.RecordNotFoundException;
 
 @SpringBootTest(classes = LocalImageService.class)
-public class LocalImageServiceTest {
+class LocalImageServiceTest {
 
 	@Autowired
 	LocalImageService localImageServ;
 
-	
 	@Test
 	void uploadImage() throws FileNotFoundException, IOException {
 		final MockMultipartFile multipartFile = new MockMultipartFile("images", "image.jpg", "image/jpeg", "random".getBytes());
